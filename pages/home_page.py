fd7e9dc2768348a46bdec686e4a54ab4f2240eee
ANanncy <nanncy.andonissamy@gmail.com>
2025-04-09 12:19:49 +0200

    envoi fichier travaille test 1

---
+++ pages/home_page.py
@@ -0,0 +1,14 @@
+from common_page import CommonPage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+
+
+class HomePage(CommonPage):
+   
+    pass
+        
+        
+        
--- steps_defs/test1_connexion.py
+++ steps_defs/test1_connexion.py
@@ -1,6 +1,9 @@
 from pytest_bdd import scenarios, given, when, then, parsers
 from pages.common_page import CommonPage
 from pages.connexion_page import Connexion
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Initialisation des variables d'environnement
 username = (By.ID, 'user-name')
@@ -8,6 +11,7 @@ password = (By.ID,'password')
 button = (By.ID,'login-button')
 saisi_username = "standard_user"
 saisi_password = "secret_sauce"
+home_page = (By.ID, 'inventory container')
 
 # Chargement du fichier .feature
 scenarios("../features/connexion.feature")
@@ -17,12 +21,22 @@ scenarios("../features/connexion.feature")
 def ouverture_nav(prep_page,browser):
     prep_page.ouverture_navigateur(url)
     
-@when(parsers.parse("je saisis mon identifiant \'{username}\' et mon mot de passe \'{password}\'"))
-def connexion(prep_page,browser, username, password, button):
-   # Pour le username
-   prep_page.renseigner_champs(username)
+@when(parsers.parse("je saisis mon identifiant \'{saisi_username}\' et mon mot de passe \'{saisi_password}\'"))
+def connexion(prep_page, browser, username, saisi_username, password, saisi_password, button):
+    # Pour le username
+    prep_page.__class__ = Connexion
+    prep_page.renseigner_champs(username, saisi_username)
     # Pour le password
-    prep_page.renseigner_champs(password)
+    prep_page.renseigner_champs(password, saisi_password)
+    prep_page.confirmer_champs(button)
+    
+@then("je suis connecte sur le site saucedemo")
+def connecte(prep_page, browser,home_page)
+    prep_page.__class__ = HomePage
+    prep_page.element_present(home_page)
+    print("je suis bien connecte")
+    
+
 
 
 
@@ -30,11 +44,7 @@ def connexion(prep_page,browser, username, password, button):
 
     
 
-@when(parsers.parse("je recherche le lien du jeu '{keyword}' dans les {research_area:d} premiers résultats de recherche"))
-def search_query(browser, search):
-    result = click_google_play_result(search, limit=10)
-    assert result.is_correct_game_displayed(search), f"Le jeu '{search}' n'est pas affiché sur la page de résultats."
-    return result
+
     
 
 @then(parsers.parse("je suis sur la page de '{keyword}' sur Google Play"))
